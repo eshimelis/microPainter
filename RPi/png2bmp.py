8e02a8bf685f837ee96370c1dc44add0e7a16592
austinchun21 <achun@g.hmc.edu>
2016-11-28 13:10:10 -0800

    Updated png2bmp: convert to RGBA and scaling

--- RPi/png2bmp.py
+++ RPi/png2bmp.py
@@ -6,21 +6,32 @@
 from PIL import Image
 
 # Load file
-file_in = "image0.png"
-img = Image.open(file_in)
+img = Image.open("image0.png")
 
-# print(img.width) 
-# print(img.height) 
+# Make sure in RGBA format
+img = img.convert("RGBA")
 
-# Scale image to max 144 pixels tall
-maxsize = (1000000, 144)
-img.thumbnail(maxsize, Image.ANTIALIAS)
-bg = Image.new("RGB", img.size, (0,0,0))
 
-bg.paste(img,img)
+# Scaling factor
+(width, height) = img.size
+# Max height of 144 pixels
+scale = 144.0 / height 
+print ("Scale: ", scale)
+newHeight = scale*height
+# Adjust horizontal resolution
+newWidth = 3.0*scale*width
 
-# print(img.width)
-# print(img.height)
+print("Height: ", newHeight, ", Width: " newWidth)
+
+
+# Scales image to max height 144, and some ratio for width to keep high resolution horizontally
+img = img.resize( (int(newWidth), int(newHeight)), Image.ANTIALIAS)
+
+# Add a background to fix bugs
+bg = Image.new("RGB", img.size, (0,0,0) )
+# Paste image on background and use a mask to fix bugs
+# # (I beleive the bugs were the transparencies)
+bg.paste(img , img)
 
 # Write out bmp file
 bg.save("image0.bmp")
