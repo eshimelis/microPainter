cee740a9319137c32eefd5bb6b3e79834d2bee4b
eshimelis <eshimelis@hmc.edu>
2016-12-05 11:09:15 -0800

    Cleaned up some things

--- site/mainServer.py
+++ site/mainServer.py
@@ -1,9 +1,4 @@
 import os
-# We'll render HTML templates and access data sent by POST
-# using the request object from flask. Redirect and url_for
-# will be used to redirect the user once the upload is done
-# and send_from_directory will help us to send/show on the
-# browser the file that the user just uploaded
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from werkzeug import secure_filename
 
@@ -12,30 +7,28 @@ app = Flask(__name__)
 
 # This is the path to the upload directory
 app.config['UPLOAD_FOLDER'] = '../RPi'
-# These are the extension that we are accepting to be uploaded
+
+# Allowed extensions
 app.config['ALLOWED_EXTENSIONS'] = set(['png'])
 
-# For a given file, return whether it's an allowed type or not
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
-# This route will show a form to perform an AJAX request
-# jQuery is loaded to execute the request and update the
-# value of the operation
+# Index
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-# Route that will process the file upload
+# File upload
 @app.route('/upload', methods=['POST'])
 def upload():
     # Get the name of the uploaded file
     file = request.files['file']
     # Check if the file is one of the allowed types/extensions
     if file and allowed_file(file.filename):
-        # Make the filename safe, remove unsupported chars
+
+        # Delete old files
         filename = "image0.png"
         oldPNG = "../RPi/image0.png"
         oldBMP = "../RPi/image0.bmp"
@@ -45,21 +38,15 @@ def upload():
             print("Found file")
             os.remove(oldBMP)
 
-        #exec(open("../RPi/png2bmp.py").read())
-        
-        # Move the file form the temporal folder to
-        # the upload folder we setup
+        # Save file
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         os.system("cd ../RPi; python png2bmp.py")
-        # Redirect the user to the uploaded_file route, which
-        # will basicaly show on the browser the uploaded file
+
+        # Redirect after upload
         return redirect(url_for('uploaded_file',
                                 filename=filename))
 
-# This route is expecting a parameter containing the name
-# of a file. Then it will locate that file on the upload
-# directory and show it on the browser, so if the user uploads
-# an image, that image is going to be show after the upload
+# After upload
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     os.system("cd ../RPi; sudo ./microPainter")
@@ -67,4 +54,4 @@ def uploaded_file(filename):
                                filename)
 
 if __name__ == "__main__":
-    app.run(host= '172.28.70.209', port=8080)
+    app.run(host= '172.28.70.209', port=9000)
